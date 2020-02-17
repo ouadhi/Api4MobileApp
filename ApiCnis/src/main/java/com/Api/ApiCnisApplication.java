a4b4d059463850bbd439b5e7b556278e4a6f5e83
ouadhi <ouadhimohammed@gmail.com>
2020-02-17 10:39:09 +0100

    ingnore same  endpoints

--- ApiCnis/src/main/java/com/Api/ApiCnisApplication.java
+++ ApiCnis/src/main/java/com/Api/ApiCnisApplication.java
@@ -4,18 +4,28 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.CommandLinePropertySource;
 
 import com.Api.Repository.TodoRepository;
 import com.Api.entity.Todo;
 
 @SpringBootApplication
-public class ApiCnisApplication    {
+public class ApiCnisApplication  implements  CommandLineRunner{
 	
 	@Autowired  
 	private  TodoRepository repository  ; 
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiCnisApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+	 Todo  todo  = new Todo()  ; 
+	 todo.setName("mohammled");
+	 
+	 repository.save(todo) ;
+		
 	}
 
 
